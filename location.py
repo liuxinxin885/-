657725d6ec372293cc4e24d595469f1c1f99da04
萝莉控 <540134749@qq.com>
2020-07-06 15:43:09 +0800

    location.py
    获取文件位置

---
+++ location.py
@@ -0,0 +1,12 @@
+import sys
+import os
+#获取当前文件位置
+def rp(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+    # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
